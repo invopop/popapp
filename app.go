2745f064f538ef764334e4d6c9ebb4efdd2780bd
Sam Lown <sam.lown@invopop.com>
2025-11-13 17:43:45 +0000

    Return of the App, clarifying responsibilities with main

---
+++ app.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+
+	"github.com/invopop/client.go/invopop"
+	"github.com/invopop/client.go/pkg/runner"
+	"github.com/invopop/popapp/internal/config"
+	"github.com/invopop/popapp/internal/domain"
+	"github.com/invopop/popapp/internal/interfaces/gateway"
+	"github.com/invopop/popapp/internal/interfaces/web"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+/*
+ * ABOUT: The app structure is used to encapsulate all the components of the application.
+ * It provides a convenient way to access and manage these components.
+ */
+
+const (
+	defaultWebPort = "8080"
+)
+
+// App keeps the main application logic in one place.
+type App struct {
+	conf   *config.Config
+	ic     *invopop.Client
+	web    *web.Service
+	gw     *gateway.Service
+	domain *domain.Setup
+}
+
+// New instantiates a new App.
+func New(conf *config.Config) *App {
+	app := new(App)
+	app.ic = invopop.New(
+		invopop.WithConfig(app.conf.Invopop),
+	)
+	app.domain = domain.New()
+	app.gw = gateway.New(app.conf.Config, app.domain)
+	app.web = web.New(app.domain)
+	return app
+}
+
+// AddCommands will add the application's own cobra commands to the provided
+// root.
+func (app *App) AddCommands(root *cobra.Command) {
+	root.AddCommand(&cobra.Command{
+		Use:   "serve",
+		Short: "Start accepting tasks and serving HTML assets",
+		Long:  "",
+		Args:  nil,
+		Run: func(_ *cobra.Command, _ []string) {
+			app.serve()
+		},
+	})
+}
+
+func (app *App) serve() {
+	app.web = web.New(app.domain)
+
+	run := new(runner.Group)
+
+	run.Start(func(_ context.Context) error {
+		return app.web.Start(defaultWebPort)
+	})
+	run.Start(func(_ context.Context) error {
+		return app.gw.Start()
+	})
+	run.Stop(func(ctx context.Context) error {
+		return app.web.Stop(ctx)
+	})
+	run.Stop(func(_ context.Context) error {
+		app.gw.Stop()
+		return nil
+	})
+
+	if err := run.Wait(); err != nil {
+		log.Error().Err(err).Msg("shutting down")
+	}
+}
--- main.go
+++ main.go
@@ -2,99 +2,30 @@
 package main
 
 import (
-	"context"
-
-	"github.com/invopop/client.go/invopop"
-	"github.com/invopop/client.go/pkg/runner"
 	"github.com/invopop/popapp/internal/config"
-	"github.com/invopop/popapp/internal/domain"
-	"github.com/invopop/popapp/internal/interfaces/gateway"
-	"github.com/invopop/popapp/internal/interfaces/web"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
-// ABOUT: The main function initializes the application.
-// It starts the web server and the gateway service.
-// This is done using a command-line interface.
+/*
+ * ABOUT: The main function initializes and passes control over to
+ * the applicaiton.
+ */
 
 const (
-	configPath     = "./config/config.yaml"
-	defaultWebPort = "8080"
+	configPath = "./config/config.yaml"
 )
 
-// ABOUT: The app structure is used to encapsulate all the components of the application.
-// It provides a convenient way to access and manage these components.
-// Any new domain config that is needed should be passed from the config object to the domain here.
-
-// App keeps the main application logic in one place.
-type App struct {
-	config *config.Config
-	ic     *invopop.Client
-	web    *web.Service
-	gw     *gateway.Service
-	domain *domain.Setup
-}
-
 func main() {
-	app := new(App)
-	// Here initialize the app components
-	app.config = config.NewConfig(configPath)
-	app.ic = invopop.New(
-		invopop.WithConfig(app.config.Invopop),
-	)
+	conf := config.NewConfig(configPath)
 
-	app.domain = domain.New() // pass as arguments the elements needed for the domain
+	app := New(conf)
 
-	app.gw = gateway.New(app.config.Config, app.domain)
-
-	app.run()
-}
-
-func (app *App) run() {
-	cmd := app.root()
-	if err := cmd.Execute(); err != nil {
+	root := &cobra.Command{Use: app.conf.Name}
+	app.AddCommands(root)
+	if err := root.Execute(); err != nil {
 		log.Fatal().Err(err).Msg("exiting")
 	}
+
 	log.Info().Msg("process terminated")
-}
-
-func (app *App) root() *cobra.Command {
-	root := &cobra.Command{Use: app.config.Name}
-
-	root.AddCommand(&cobra.Command{
-		Use:   "serve",
-		Short: "Start accepting tasks and serving HTML assets",
-		Long:  "",
-		Args:  nil,
-		Run: func(_ *cobra.Command, _ []string) {
-			app.serve()
-		},
-	})
-
-	return root
-}
-
-func (app *App) serve() {
-	app.web = web.New(app.domain)
-
-	run := new(runner.Group)
-
-	run.Start(func(_ context.Context) error {
-		return app.web.Start(defaultWebPort)
-	})
-	run.Start(func(_ context.Context) error {
-		return app.gw.Start()
-	})
-	run.Stop(func(ctx context.Context) error {
-		return app.web.Stop(ctx)
-	})
-	run.Stop(func(_ context.Context) error {
-		app.gw.Stop()
-		return nil
-	})
-
-	if err := run.Wait(); err != nil {
-		log.Error().Err(err).Msg("shutting down")
-	}
 }
